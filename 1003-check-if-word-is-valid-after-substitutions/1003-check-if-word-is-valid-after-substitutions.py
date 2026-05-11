cf4a92b220fd23ebd4d2f99615b37c5766a542f2
Maria Jose <4mariajo@gmail.com>
2026-05-11 10:03:47 +0530

    Time: 70 ms (34.61%), Space: 12.7 MB (17.31%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.py
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.py
@@ -4,7 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        while "abc" in s:
-            s = s.replace("abc", "")
+        stack = []
 
-        return s == ""
+        for ch in s:
+            stack.append(ch)
+
+            # if last 3 letters are abc
+            if stack[-3:] == ['a', 'b', 'c']:
+                stack.pop()
+                stack.pop()
+                stack.pop()
+
+        return len(stack) == 0
