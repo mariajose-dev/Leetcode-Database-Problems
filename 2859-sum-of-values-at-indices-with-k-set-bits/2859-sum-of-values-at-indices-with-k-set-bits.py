b512c1bf4728d84ed955c3414b63d372641136f9
Maria Jose <4mariajo@gmail.com>
2026-05-13 12:34:05 +0530

    Time: 9 ms (43.16%), Space: 12.6 MB (23.16%) - LeetHub

--- 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.py
+++ 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.py
@@ -6,13 +6,9 @@ class Solution(object):
         :rtype: int
         """
         ans=[0]*(len(nums))
-
+        sum=0
         for i in range(0,len(nums)):
             ans[i]=ans[i>>1]+(i&1)
-        print(ans)
-        sum=0
-        for i in range(len(ans)):
             if ans[i]==k:
                 sum+=nums[i]
-        print(sum)
         return sum
