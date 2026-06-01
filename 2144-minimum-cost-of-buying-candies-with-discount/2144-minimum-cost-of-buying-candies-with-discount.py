47e092000362e5345791342087582989c892e48f
Maria Jose <4mariajo@gmail.com>
2026-06-01 19:37:41 +0530

    Time: 0 ms (100%), Space: 12.5 MB (18.75%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
@@ -5,7 +5,6 @@ class Solution(object):
         :rtype: int
         """
         cost.sort(reverse=True)
-        print(cost)
         sum=0
         for i in range(1,len(cost)+1):
             if i%3==0:
