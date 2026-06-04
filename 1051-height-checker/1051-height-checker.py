51b2fe8a751072c4dd502e5c613e1921bd22ca68
Maria Jose <4mariajo@gmail.com>
2026-06-04 22:45:25 +0530

    Time: 3 ms (19.93%), Space: 12.5 MB (17.28%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -10,6 +10,6 @@ class Solution(object):
         heights.sort()
         co=0
         for i in range(len(heights)):
-            if exp[i]!=heights[i]:
+            if exp[i] is not heights[i]:
                 co+=1
         return co
