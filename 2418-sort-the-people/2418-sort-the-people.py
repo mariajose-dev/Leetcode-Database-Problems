716b7e7cba08740d7475d03a72605daf94c2a52c
Maria Jose <4mariajo@gmail.com>
2026-06-11 21:26:44 +0530

    Time: 3 ms (90.6%), Space: 12.7 MB (44.97%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def sortPeople(self, names, heights):
+        """
+        :type names: List[str]
+        :type heights: List[int]
+        :rtype: List[str]
+        """
+        dic={}
+        for i in range(len(names)):
+            dic[heights[i]]=names[i]
+        lis=[]
+        for x in sorted(dic):
+            lis.append(dic[x])
+        return lis[::-1]
