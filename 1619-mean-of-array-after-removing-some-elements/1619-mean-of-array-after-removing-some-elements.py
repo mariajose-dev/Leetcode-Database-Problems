e2e5b8239fd5c23305484df4356e879725c65b9d
Maria Jose <4mariajo@gmail.com>
2026-06-26 20:50:04 +0530

    Time: 3 ms (58.14%), Space: 12.3 MB (93.02%) - LeetHub

---
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def trimMean(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: float
+        """
+        arr.sort()
+
+        k = len(arr) // 20
+
+        arr2 = arr[k:len(arr)-k]
+
+        return float(sum(arr2)) / len(arr2)
