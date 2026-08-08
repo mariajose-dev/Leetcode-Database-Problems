f796f13aa645479c76b49a7534e8d778af7caacb
Maria Jose <4mariajo@gmail.com>
2026-08-08 20:13:59 +0530

    Time: 18 ms (38.3%), Space: 12.5 MB (18.3%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -7,9 +7,9 @@ class Solution(object):
         ans=""
         for i in range(len(command)):
             if command[i]=='G':
-                ans+="G"
-            if command[i]=='(' and command[i+1]==")":
+                ans+='G'
+            if command[i]=='(' and command[i+1]==')':
                 ans+="o"
-            if command[i]=='(' and command[i+1]=="a"and command[i+2]=="l" and command[i+3]==')':
+            if command[i]=='(' and command[i+1]=='a' and command[i+2]=='l' and command[i+3]==')':
                 ans+="al"
         return ans
