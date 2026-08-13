509fca5ff137b465dec7438116564e87d497e288
Maria Jose <4mariajo@gmail.com>
2026-08-13 23:19:54 +0530

    Time: 8 ms (44.26%), Space: 12.5 MB (48.77%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.py
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.py
@@ -5,7 +5,8 @@ class Solution(object):
         :rtype: str
         """
         co=0
-        for x in num[::-1]:
+        lis=num[::-1]
+        for x in lis:
             co+=1
             if x!='0':
                 break
