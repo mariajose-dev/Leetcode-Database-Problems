385934c340905a6853a567634cccbbf6065beded
Maria Jose <4mariajo@gmail.com>
2026-08-17 10:03:03 +0530

    Time: 4 ms (36.18%), Space: 12.4 MB (19.74%) - LeetHub

--- 3158-find-the-xor-of-numbers-which-appear-twice/3158-find-the-xor-of-numbers-which-appear-twice.py
+++ 3158-find-the-xor-of-numbers-which-appear-twice/3158-find-the-xor-of-numbers-which-appear-twice.py
@@ -4,25 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        dic={}
-        for x in nums:
-            if x in dic:
-                dic[x]+=1
-            else:
-                dic[x]=1
-        lis=[]
-        for x in dic:
-            if dic[x]==2:
-                lis.append(x)
-                
-        if len(lis)<1:
-            return 0
-        if len(lis)==1:
-            return lis[0]
-        if len(lis)>1:
-            res=lis[0]
-            for i in range(1,len(lis)):
-                res^=lis[i]
-            return res
-
+        dup=0
+        x=[]
+        for i in nums:
+            if i in x:
+                dup=dup^i
+            x.append(i)
+        return dup
 
