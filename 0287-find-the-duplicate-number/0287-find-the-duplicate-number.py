ce237cc452d61e97abe5e48cd884c1f8625b7f7e
Maria Jose <4mariajo@gmail.com>
2026-08-18 11:43:38 +0530

    Time: 202 ms (5%), Space: 20.7 MB (84.49%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l,r=1,len(nums)
+        while l+1<=r:
+            mid,count=(l+r)//2,0
+            for num in nums:
+                if num<=mid: count +=1
+            if count<=mid:
+                l=mid+1
+            else:
+                r=mid
+        return r
